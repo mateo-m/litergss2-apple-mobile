5001656f0dd9fcdca6e1a0422924138084b0994d
Brice BULGARELLI <brice.bulgarelli@gmail.com>
2021-02-28 14:26:56 +0100

    #15 refactor: smarter DisplayWindow reload + remove references to old Config module

--- ext/LiteRGSS/DisplayWindowConfigLoader.h
+++ ext/LiteRGSS/DisplayWindowConfigLoader.h
@@ -6,16 +6,7 @@
 
 class DisplayWindowConfigLoader {
 public:
-	cgss::DisplayWindowSettings load() const;
 	cgss::DisplayWindowVideoSettings loadVideoFromData(long width, long height, double scale, long bitsPerPixel) const;
-	cgss::DisplayWindowContextSettings loadContext() const;
-private:
-	cgss::DisplayWindowVideoSettings loadVideoFromConfigs() const;
-	bool loadSmoothScreenFromConfigs() const;
-	std::string loadTitleFromConfigs() const;
-	unsigned int loadFrameRateFromConfigs() const;
-	bool loadVSYNCFromConfigs() const;
-	bool loadFullScreenFromConfigs() const;
 };
 
 #endif
